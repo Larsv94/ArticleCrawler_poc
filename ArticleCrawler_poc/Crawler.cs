79ed8c97a13561db4df7c8f290379f333a8d6c1b
Lars <larsvolkers@hotmail.nl>
2018-05-22 07:55:48 +0200

    Switching to desktop -

--- ArticleCrawler_poc/Crawler.cs
+++ ArticleCrawler_poc/Crawler.cs
@@ -1,17 +1,35 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ArticleCrawler_poc
 {
     class Crawler
     {
+        private HtmlWeb _webReader;
+        private string BaseURL;
         
-        public Crawler()
+        public HtmlWeb WebReader
         {
-
+            get { return _webReader ?? (_webReader = new HtmlWeb()); }
         }
 
+        public HtmlDocument BaseDocument
+        {
+            get { return WebReader.Load(BaseURL); }
+        }
+
+        public Crawler(String BaseURL)
+        {
+            this.BaseURL = BaseURL;
+        }
+
+        public async void StartCrawling(int depth)
+        {
+            int amount = depth % 20;
+        }
 
     }
 }
--- ArticleCrawler_poc/Models/Article.cs
+++ ArticleCrawler_poc/Models/Article.cs
@@ -13,18 +13,18 @@ namespace ArticleCrawler_poc.Models
         public int ArticleID { get; set; }
 
         [Index(IsUnique =true)]
+        [Required]
         public string Title { get; set; }
-
+        [Required]
         public string Text { get; set; }
         public string Abstract { get; set; }
-
-
-
+        
         public DateTime PublishedOn { get; set; }
 
         
 
         public virtual ICollection<ArticleCategorie> Categories { get; set; }
+        [Required]
         public virtual Writer Writer { get; set; }
         public virtual ExternalLink Links { get; set; }
         public virtual GenerationInfo GenerationInfo { get; set; }
